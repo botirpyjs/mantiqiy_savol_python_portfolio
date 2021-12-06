7d6542ae67b9a733e38abc38c092779f9b3001fb
Botir Akhmedov <95535127+botirpyjs@users.noreply.github.com>
2021-12-06 20:14:35 +0300

    python portfolio

---
+++ mantiqiy savol.py
@@ -0,0 +1,11 @@
+print("3 xonali hohlagan son o'ylab uni pastga yozing")
+print("asosiy shart o'ylagan sonni 3 ga ko'paytiring \n javobga 9 ni qo'shing, uning javobiga 15 ni qo'shing \n javobdan 3ni ayrib, keyin esa 3 ga bo'ling.\n Javobga esa birinchi o'ylagan sonni ayring.\n Javob 7 chiqdimi demak men to'g'ri kod yozgan ekanman ;)")
+ism = int(input("3 xonali son "))
+a = ism*3
+b = a+9
+c = b+15
+d = c-3
+e = d//3
+f = e-ism
+print(f"JAVOB >>>> {f}")
+
